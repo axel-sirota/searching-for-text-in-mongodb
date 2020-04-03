c616198a35aeee4afddb3a3ea44b2474ac603c90
Axel Sirota <axel.sirota@gmail.com>
2020-04-03 17:15:01 -0300

    Needing to change the dataset due to legal thingys
    adapt
    
    compressing

--- demo-module2.js
+++ demo-module2.js
@@ -1,27 +1,45 @@
-// First run mongoimport -c movies -d pluralsight --file movieDetails.json to populate the DB
+// First run mongoimport -d pluralsight -c rent --file newyork-rent.csv --type csv --headerline --drop
 
 //Running a query, verify it is exact match
 
-db.movies.find({title: "Toy story 3"})
+db.rent.find({name: "Tribeca"})
 
 // Create the index
 
 db.movies.createIndex({title: "text"})
 
-// Running the same query, nos is full text search.
+// Running the same query, now is full text search.
 
-db.movies.find({$text : {$search : "Toy Story"}}, {title: 1})
+> db.rent.find({$text: {$search: "Tribeca"}}, {_id:0, "name":1})
 
-// Get only the Toy Story movies
+// Get only Loft apaertment in tribeca!
 
-db.movies.find({$text : {$search : "\"Toy Story\""}}, {title: 1})
+> db.rent.find({$text: {$search: "\"Tribeca\" loft"}}, {_id:0, "name":1}) // check the order does not matter!
+
+// Confirm that by lowercase and stemming, we get the same results with Tribeca, TriBeca and TriBeCa
+
+> db.rent.find({$text: {$search: "Tribeca"}}).count()
+185
+> db.rent.find({$text: {$search: "TriBeca"}}).count()
+185
+> db.rent.find({$text: {$search: "TriBeCa"}}).count()
+185
 
 
-// Get movies with a title having "return" and NOT having "wars"
+// Getting rentals in tribeca that ARE NOT lofts
 
-db.movies.find({$text : {$search : "\"return\" -wars"}}, {title: 1})
+db.rent.find({$text : {$search : "Tribeca -loft"}}, {name: 1})
 
-// Get movies with a title having (return OR star) but NOT having "wars"
+// Getting rentals in Tribeca or East Village that are not Lofts!
 
 
-db.movies.find({$text : {$search : "return star -wars"}}, {title: 1})
+db.rent.find({$text : {$search : "Tribeca \"East Village\" -loft"}}, {name: 1})
+
+// Going crazy, explaining:
+
+> db.rent.find({$text : {$search : "\"bedroom\" apartment"}}, {name: 1}).count()
+8719
+> db.rent.find({$text : {$search : "bedroom"}}, {name: 1}).count()
+8701
+> db.rent.find({$text : {$search : "\"bedroom\" apartment -bedroom"}}, {name: 1}).count() // What is going on here?!
+18
--- demo-module5.js
+++ demo-module5.js
@@ -1,2 +1,7 @@
+// Lets import the data mongoimport -d pluralsight -c countries --drop --file countries.json
+
 // Always run `use pluralsight` to use the correct DB
+
+db.countries.findOne()
+
 
