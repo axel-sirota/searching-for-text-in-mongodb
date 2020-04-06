44daa9e75782dc32bddc486c71b6427a9dbf522f
Axel Sirota <axel.sirota@gmail.com>
2020-04-06 00:49:15 -0300

    Demo module 4

--- demo-module4.js
+++ demo-module4.js
@@ -1,24 +1,44 @@
 // Always run `use pluralsight` to use the correct DB
 
-// /Let's search for jennifer aniston movies!
+// /Let's search for lofts in tribeca!
 
-db.movies.find({$text: {$search: "jennifer aniston"}}, {_id:0, title:1, plot:1})
+// First lets drop the previous indexes and create a new one!
 
-// Ordering by score to be less random!
+db.rent.dropIndexes()
+db.rent.createIndex({"name": "text", "neighbourhood_cleansed": "text", "description": "text"})
 
-db.movies.find({$text: {$search: "jennifer aniston"}}, {_id:0, title:1, plot:1, score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
+db.rent.find({$text: {$search: "tribeca loft"}}, {_id:0, name:1, neighbourhood_cleansed:1})
 
-// We see a lot of movies where she did not star! However the DB is not normalized (Sometimes is Jennifer Aniston, sometimes Jenifer Aniston)
+// Seems odd, and many results are not from tribeca!
 
-// As she is an actress, lets try to give actors more weight!
+db.rent.find({$text: {$search: "tribeca loft"}}, {_id:0, name:1, neighbourhood_cleansed:1, score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
+
+// That looks better! However if we check there are lots of NOT tribeca results with high score!
+
+db.rent.find({$text: {$search: "tribeca loft"}, neighbourhood_cleansed: {$ne: "Tribeca"}}, {_id:0, name:1, neighbourhood_cleansed:1, score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
+
+// Lets give more weight to neighborhood!
 
 db.movies.dropIndexes()
 
-db.movies.createIndex({"$**": "text"}, { weights: {"actors" :5}})
+db.rent.createIndex({"name": "text", "neighbourhood_cleansed": "text", "description": "text"}, { weights: {neighbourhood_cleansed:5}})
 
-// We see now that more Jennifers appear! Lets filter that by surname
+// Noticed how long it starts to take?
 
-db.movies.find({$text: {$search: "jennifer \"aniston\""}}, {_id:0, title:1, plot:1, score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
+db.rent.find({$text: {$search: "tribeca loft"}, neighbourhood_cleansed: {$ne: "Tribeca"}}, {_id:0, name:1, neighbourhood_cleansed:1, score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
 
-db.movies.explain("executionStats").find({$text: {$search: "jennifer \"aniston\""}}, {_id:0, title:1, plot:1, score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
+// Now it looks better! we can make a cut in our app for score > 5 for example!
 
+//Maybe a better strategy will be to partition the index (but loose the ability of searching full text in neighborhoods!)
+
+db.movies.dropIndexes()
+db.rent.createIndex({"neighbourhood_cleansed":1, "name": "text", "description": "text"})
+
+db.rent.explain("executionStats").find({$text: {$search: "loft"}, neighbourhood_cleansed: "Tribeca"}, {_id:0, name:1, neighbourhood_cleansed:1, score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
+
+// What happens if we try to break the rules? If we try to fo a full text but the other indexed field put in negation
+
+db.rent.find({$text: {$search: "tribeca loft"}, neighbourhood_cleansed: {$ne: "Tribeca"}}, {_id:0, name:1, neighbourhood_cleansed:1, score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
+
+// ERROR!! Of course, we know that the text index is supedited to the first hashtable! So Mongo complains
+// if it has to go search full text in many places! 
